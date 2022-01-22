f975349940840297b46b443e01bce15a8310eabb
adrianooliveira122 <95711305+adrianooliveira122@users.noreply.github.com>
2022-01-22 11:04:13 -0300

    Update and rename README.md to cadastroEvento.js

---
+++ cadastroEvento.js
@@ -0,0 +1,21 @@
+# cadastroEvento
+ let idade = 18
+if (idade>=18){
+    console.log("Cadastro permitido")
+}else {
+    console.log ("Menor de 18 anos, cadastro não permitido")
+} let dataevento = "28/12/2021", dataAtual = "25/12/2021"
+
+if(dataevento >= dataAtual) {
+    console.log(" realize o cadastro") 
+    }else {
+    console.log ("o cadastro nao será permitido, porque a data ser invalida !!")
+} 
+let quantidade = 100
+if(quantidade <=100) {
+    console.log ("Cadastro realizado")
+}else {
+    console.log ("Excedeu a quantidade de vagas disponiveis")
+}
+const participantes = ["lucas,pedro,jose,maria,antonio"]
+console.log (participantes)
